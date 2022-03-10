62f21e71721a43f1932b38475dfd1d67d5d0d651
ozeliurs <ozeliurs@gmail.com>
2022-03-10 23:04:07 +0100

    newline fix

--- www/update.py
+++ www/update.py
@@ -1,4 +1,4 @@
 import requests
 
 with open("/data/hothothot.log", "a", encoding="utf8") as log:
-  log.write(requests.get("https://hothothot.dog/api/capteurs/").text)
+    log.write(requests.get("https://hothothot.dog/api/capteurs/").text + "\n")
