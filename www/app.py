85e126b087a0609c1cb51e09c28ecb903aa867ba
BoLLYde Maxime <ozeliurs@gmail.com>
2022-03-10 22:53:37 +0100

    Update app.py

--- www/app.py
+++ www/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import json
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +8,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route('/hothothot/capteurs')
+def hothothot():
+    with open("hothothot.log", "r", encoding="utf8") as log:
+        hist = log.read()
+        
+    return jsonify(json.loads(hist.split("\n")[-1]))
+
+
+@app.route('/hothothot/hist')
+def hothothot_hist()
+    with open("hothothot.log", "r", encoding="utf8") as log:
+        hist = log.read()
+        hist = [json.loads(x) for x in hist.split("\n")[-48:]]
+    
+    return jsonify(hist)
 
 
 if __name__ == '__main__':
