a16001c99e4c3a223908a57d1b7806475c69d2f3
BoLLYde Maxime <ozeliurs@gmail.com>
2022-06-11 14:34:34 +0200

    fix fix migration

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 
-sudo touch /var/opt/api/tmp
-sudo rm -r /var/opt/api/*
+sudo touch /opt/api/tmp
+sudo rm -r /opt/api/*
 
-sudo cp -r /tmp/api.ozeliurs.com/www/* /var/opt/api/
+sudo cp -r /tmp/api.ozeliurs.com/www/* /opt/api/
 
-sudo pip install -r /var/opt/api/requirements.txt
+sudo pip install -r /opt/api/requirements.txt
 
-sudo chown -R www-data /var/opt/api/
+sudo chown -R www-data /opt/api/
 
 sudo rm /etc/systemd/system/api.ozeliurs.com.service
 sudo cp /tmp/api.ozeliurs.com/api.ozeliurs.com.service /etc/systemd/system/
